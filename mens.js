fac047f81a32f0f3426c7150b82ec53437138a24
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-03-23 23:49:04 +0300

    Declare array

---
+++ mens.js
@@ -0,0 +1,31 @@
+var mens =[{name: "T-shirt",
+              id: "clothe1-btn",
+              sizes:[{name:"S",price:600},{name:"M",price:900},{name:"L",price:1300},{name:"XL",price:1300}],
+              colors:[{name:"Black"},{name:"Red"},{name:"Blue"},{name:"Beige"},{name:"Brown"}]},
+              
+              {name: "Blazer",
+              id: "clothe2-btn",
+              sizes:[{name:"S",price:600},{name:"M",price:900},{name:"L",price:1300},{name:"XL",price:1300}],
+              colors:[{name:"Black"},{name:"Red"},{name:"Blue"},{name:"Beige"},{name:"Brown"}]},
+
+              {name: "Shirt",
+              id: "clothe3-btn",
+              sizes:[{name:"S",price:600},{name:"M",price:900},{name:"L",price:1300},{name:"XL",price:1300}],
+              colors:[{name:"Black"},{name:"Red"},{name:"Blue"},{name:"Beige"},{name:"Brown"}]}, 
+
+              {name: "Rubber",
+              id: "shoes1-btn",
+              sizes:[{name:"S",price:600},{name:"M",price:900},{name:"L",price:1300},{name:"XL",price:1300}],
+              colors:[{name:"Black"},{name:"Red"},{name:"Blue"},{name:"Beige"},{name:"Brown"}]},
+
+              {name: "Official",
+              id: "shoes2-btn",
+              sizes:[{name:"S",price:600},{name:"M",price:900},{name:"L",price:1300},{name:"XL",price:1300}],
+              colors:[{name:"Black"},{name:"Red"},{name:"Blue"},{name:"Beige"},{name:"Brown"}]},
+              
+              {name: "Sneakers",
+              id: "shoes3-btn",
+              sizes:[{name:"S",price:600},{name:"M",price:900},{name:"L",price:1300},{name:"XL",price:1300}],
+              colors:[{name:"Black"},{name:"Red"},{name:"Blue"},{name:"Beige"},{name:"Brown"}]},
+            ];
+
