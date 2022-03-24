68c36b75b83f0c2b7fed2033652a0d64ebbdeb18
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-03-24 03:50:42 +0300

    add display functionality

---
+++ women.js
@@ -0,0 +1,20 @@
+
+// get the modal
+// var modal = document.getElementById("modal");
+// // button that opens modal
+// var btn = document.getElementById("btn");
+// // user opening modal
+// btn.onclick = function(event) {
+//     event.preventDefault();
+//   modal.style.display = "block";
+// }
+ function displaySummary() {
+   let x =  document.getElementById("summary")
+    let y = document.getElementById("orderSummary")
+
+      x.style.display = "block"
+      y.style.display = "none"
+ }
+
+    
+
