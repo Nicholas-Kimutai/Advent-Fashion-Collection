30a5e96f8106960ca6dda19e4cfac6a8a2b4ff16
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-03-24 11:32:39 +0300

    Add a checkout functionality

--- mens.js
+++ mens.js
@@ -42,3 +42,86 @@ Men.prototype.menColors = function(){
         col = color.name;
     });
 }
+
+$(document).ready(function(){
+    var total = 0;
+    var sizeInput = 0;
+    var colorInput = 0;
+    var netTotal = 0;
+    mens.forEach(function(men){
+        $("button").click(function(){
+            if(this.id === men.id){
+                $("#orderName").text(men.name);
+
+                var menSize = "";
+                var menColors = "";
+
+                $("#myOrders").click(function() {
+                    men.sizes.forEach(function(size){
+                        var isChecked = $("#"+size.name).is(':checked');
+                        if(isChecked){
+                            $("#"+size.name+"-"+"price").text(size.price);
+                            sizeInput = size.price;
+                            menSize = size.name;
+                        }
+                    });
+                    
+                    men.colors.forEach(function(color){
+                        var isChecked = $("#"+color.name).is(':checked');
+                        if(isChecked){
+                            $("#"+color.name+"-"+"price").text(color.price);
+                            colorInput = color.price;
+                            if(!menColors.includes(color.name)){
+                                menColors.push(color.name);
+                            }
+                        }
+                    })
+                    total = sizeInput + colorInput;
+                   
+                   
+                    $("#totalPrice").text( "Ksh."+ total); 
+                });
+
+                $("form#myOrders").submit(function(event){
+                    
+                    event.preventDefault();
+                    netTotal += total;
+                    console.log(netTotal)
+                    var menChoice = new Men(men.name,menSize,menColors,total);
+
+                    $("#nameOrder").text( menChoice.name); 
+                    $("#priceOrder").text( "Ksh." + menChoice.total); 
+                    $("#notordered").hide();
+                    $(".table").show();
+                    $("#total-orders").append('<tr><td id="menname">'+menChoice.name +'</td><td id="mensize">' + menChoice.size + '</td><td id="mencolor">' +menChoice.colors+'</td><td id="menprice">'+menChoice.total+'</td></tr>');
+                    $("#mentotalprice").text("Your total order amount is: " + netTotal);
+
+                    $('input[name="size"]').prop('checked', false);
+                    $('input[name="color"]').prop('checked', false);
+                    $(".order-btn").show()
+                }); 
+            }
+        })
+    });
+
+    $("button#proceedbtn").click(function(){
+        $(".checkout").show();
+        $("form#checkoutfrm").submit(function(event){
+            event.preventDefault();
+            var nameInput = $("input#username").val();
+            var locationInput = $("input#location").val();
+            var phoneInput = $("input#phone").val();
+            
+            console.log(nameInput)
+            console.log(locationInput)
+            console.log(phoneInput)
+            if(nameInput !== "" && locationInput !== "" && phoneInput !== ""){
+                alert("Hi "+ nameInput+ ","+" We have received your order and our rider will be delivering it to: " + locationInput);
+            }
+            $("input#username").val("");
+            $("input#location").val("");
+            $("input#phone").val("");
+        });
+    })
+});
+
